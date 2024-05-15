f5f7ec10c3c533fb49c4cefbc897db717e1a9685
liarcrown <liarcrown@naver.com>
2024-05-15 15:49:16 +0900

    feat: finish making todovesrion 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
-import TodoTemplates from "./components/TodoTemplates";
+import TodoTemplate from "./components/TodoTemplates";
 import TodoHead from "./components/TodoHead";
 import TodoList from "./components/TodoList";
 import TodoCreate from "./components/TodoCreate";
+import { TodoProvider } from "./TodoContext";
 
-const GlobalStyle = createGlobalStyle`body {
+const GlobalStyle = createGlobalStyle`
+  body {
     background: #e9ecef;
   }
 `;
 
 function App() {
   return (
-    <>
+    <TodoProvider>
       <GlobalStyle />
-
-      <TodoTemplates>
-        <TodoHead></TodoHead>
-        <TodoList></TodoList>
-        <TodoCreate></TodoCreate>
-      </TodoTemplates>
-    </>
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </TodoProvider>
   );
 }
 
